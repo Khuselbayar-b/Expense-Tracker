ce007fe012c841937ab052a9a3a3e7dd9fdc5fc4
Khuselbayar-b <kb40@illinois.edu>
2023-09-28 23:43:18 -0500

    project add

---
+++ Backend/app.js
@@ -0,0 +1,20 @@
+const express = require('express')
+const cors = require('cors');
+const app = express()
+
+require('dotenv').config()
+
+const PORT = process.env.PORT
+
+//middleware
+app.use(express.json())
+app.use(cors())
+app.get('/', (req, res)=> {
+    res.send('Hello')
+})
+const server = () => {
+    app.listen(PORT, () => {
+        console.log('listening port:', PORT)
+    })
+}
+server()
